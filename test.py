7c537064650ce0448de874d2b70e0572ff721d2a
jonasnunes.devops@senaifatesg.com <jonasnunes.devops@senaifatesg.com>
2022-06-30 08:28:46 -0400

    Alterando o cabecalho

--- test.py
+++ test.py
@@ -19,9 +19,9 @@ class Test(unittest.TestCase):
         result = self.app.get('/') 
 
         # verifica o retorno do conteudo da pagina
-        self.assertRegex(result.data.decode(), "Escreva uma Mensagem para o Cabecalho da Pagina.")
+        self.assertRegex(result.data.decode(), "Pipeline DevOPs - Jonas Junior.")
 
-
+''
 if __name__ == "__main__":
     print ('INICIANDO OS TESTES')
     print('----------------------------------------------------------------------')
